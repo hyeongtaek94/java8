8668d8086735e7a8ebe537aef4f299f39dc7baab
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-16 15:13:33 +0900

    3일차: 증감연산자

---
+++ 1장/src/java8/Day0308.java
@@ -0,0 +1,56 @@
+package java8;
+
+public class Day0308 {
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+		System.out.println();
+		
+		a++;	//전위, 전치 연산자
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		System.out.println();
+		
+		++a;	//후위, 후치 연산자
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		System.out.println();
+		
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		System.out.println();
+		
+		a = 10;
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println();
+		
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println();
+		
+		System.out.println(a);
+	}
+}
