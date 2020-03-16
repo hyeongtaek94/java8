5c83a2990208e9da037e4066bb7187313462e9dc
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-16 15:25:23 +0900

    3일차: 삼항연산자(조건연산자)

---
+++ 1장/src/java8/Day0309.java
@@ -0,0 +1,16 @@
+package java8;
+
+public class Day0309 {
+	public static void main(String[] args) {
+		int daegu = 2_000_000;
+		int global = 1_000_000;
+		
+		String str = ((daegu >= global) ? "대구광역시" : "대구시");
+		
+		System.out.println(str);
+		
+		
+		
+
+	}
+}
