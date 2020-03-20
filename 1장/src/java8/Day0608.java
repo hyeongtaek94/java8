6bc004ed075c91de4f4e4544f079d02f55444800
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-20 16:26:05 +0900

    6일차 : 반복문(while문)<2>

---
+++ 1장/src/java8/Day0608.java
@@ -0,0 +1,11 @@
+package java8;
+
+public class Day0608 {
+	public static void main(String[] args) {
+		int i = 0;
+		while(true) {	//조건에 true을 입력하면 무한반복문이 됩니다.
+			System.out.println("Hello");
+			i++;
+		}
+	}
+}
