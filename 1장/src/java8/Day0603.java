56157266953c492f7ac5a8b769c385b91dfbbb52
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-20 14:43:39 +0900

    6일차 : 반복문 (for)<3>

---
+++ 1장/src/java8/Day0603.java
@@ -0,0 +1,13 @@
+package java8;
+
+public class Day0603 {
+	public static void main(String[] args) {
+		for(int i = 0 ; i < 10 ; i++) {	//for문의 가장 기본식
+			System.out.println(i);
+		}
+		
+		for(int i = 0 ; i <5 ; i++) {
+			System.out.println("hello!");
+		}
+	}
+}
