51cfcf4e0814ff9f350b5126903baa5f69a35298
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-20 16:32:27 +0900

    6일차 : 반복문 (continue문)

---
+++ 1장/src/java8/Day0611.java
@@ -0,0 +1,15 @@
+package java8;	//continue
+
+public class Day0611 {
+	public static void main(String[] args) {
+		int i;
+		for(i= 1; i<=10; i++) {
+					if(i%2 != 0) {
+								continue;
+					
+					}
+					System.out.println(i);
+		}
+		
+	}
+}
