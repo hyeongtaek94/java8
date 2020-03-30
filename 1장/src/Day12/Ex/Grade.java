f89fd43b7dcd0ffad045931e0db8798bfa9470fa
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-30 16:28:53 +0900

    12일차 : 클래스와 객체 (2)

---
+++ 1장/src/Day12/Ex/Grade.java
@@ -0,0 +1,22 @@
+package Day12.Ex;
+
+public class Grade {
+	//필드
+	int kor, eng, mat;
+	
+	
+	//생성자
+	Grade(int kor, int eng, int mat){
+		this.kor = kor;
+		this.eng = eng;
+		this.mat = mat;
+		
+	}
+	
+	//메서드
+	double avg() {
+		int sum = kor + eng + mat;
+		return sum / 3.0;
+	}
+	
+}
---
+++ 1장/src/Day12/PackEx.java
@@ -0,0 +1,23 @@
+package Day12;
+
+import java.util.*;	//util 다음에 *를쓴다면 util에 모든것을 쓸수있음
+
+public class PackEx {
+	public static void main(String[] args) {
+		Random rd = new Random();
+		// 원칙은 java.util.Random rd = new java.util.Random();
+		// 이렇게써야 정상인데 길어서 import(가져오기)로 대체
+		// 길게 쓰면 위에 import는 없음.
+
+	
+//	시스템 패키지 종류
+//	applet은 인터넷에서 가져오는거 지금은 안씀
+	
+		
+//	classpass 윈도우즈 시스템의 환경변수 path는 DOS환경에서
+//	커맨드가 들어 있는 폴더를 알려주는 역할을 한다.
+//	환경변수 classpath는 자바 시스템에게 수행하고자
+//	하는 클래스가 어느 폴더에 있는지 알려주는 역할을 한다
+	
+	}
+}
