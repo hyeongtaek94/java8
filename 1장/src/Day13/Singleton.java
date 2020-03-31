9572da51f34e8b5f7d824ca02d734195e32b18a9
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-03-31 15:19:20 +0900

    13일차 : Singleton()메소드

---
+++ 1장/src/Day13/Singleton.java
@@ -0,0 +1,13 @@
+package Day13;	//Singleton
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {
+		
+	}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+}
