506c0ecc9732ea6aed678c4c2c313f1b7e5bbbb2
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-01 14:57:17 +0900

    14일차 : 강제 타입 변환(Casting)

--- 1장/src/Day14/PromoEx.java
+++ 1장/src/Day14/PromoEx.java
@@ -12,5 +12,20 @@ public class PromoEx {
 		pp.method2();
 //		pp.method3();	//부모의 형태이기 때문에 부모에는 method3가 없습니다.
 						//그래서 사용이 불가능합니다
+//		pc = pp;	//자식에 부모를 대입할 수 없습니다.
+		
+		pc = (PromoChild)pp;	//자식에 부모를 대입할 수 없습니다. 강제형변환은 가능합니다.
+		pc.method1();	//부모의 객체가 자식의 객체에 대입될 때는 원래부터 자식 객체였어야만 합니다.
+		pc.method2();
+		pc.method3();
+		
+		PromoParent pp2 = new PromoParent();
+		if(pp  instanceof PromoChild) {
+			pc = (PromoChild)pp;	//강제형변환은 원래의 형태로 복구만 가능합니다.
+			pc.method3();
+			
+		} else {
+			System.out.println("강제형변환을 사용할 수 없습니다.");
+		}
 	}
 }
