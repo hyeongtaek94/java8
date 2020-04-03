0a4ee1cfe30f3d2db89389f094330b307b482d43
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-03 16:11:49 +0900

    16일차 : toString추가

--- 1장/src/Day16/Key.java
+++ 1장/src/Day16/Key.java
@@ -1,5 +1,29 @@
-package Day16;
+package Day16;	//hash코드와 equals를 재정의 사용
 
 public class Key {
+	public int number;
+	public Key(int number) {
+			this.number = number;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Key) {
+			Key compareKey = (Key) obj;
+			if(this.number == compareKey.number) {
+					return true;
+			}
+		}
+		return false;
+	}
+//	
+//	@Override
+//	public int hashCode(){
+//		return number;
+		
+		@Override
+		public String toString() {
+			return "Key 객체에 저장된 Number: " + number;
+		}
+	}
 
-}
--- 1장/src/Day16/KeyEx.java
+++ 1장/src/Day16/KeyEx.java
@@ -1,4 +1,4 @@
-package Day16;
+package Day16;	//hash코드와 equals를 재정의 사용
 
 public class KeyEx {
 	public static void main(String[] args) {
@@ -9,5 +9,8 @@ public class KeyEx {
 		System.out.println(k1.equals(k2));
 		System.out.println(k1.hashCode());
 		System.out.println(k2.hashCode());
+		
+		
+		System.out.println(k1);
 	}
 }
