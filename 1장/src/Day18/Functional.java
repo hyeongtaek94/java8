d78d4b976c04bb03785f40942e931b464d20e2bd
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-07 13:41:10 +0900

    18일차 : 익명구현객체와 람다식

---
+++ 1장/src/Day18/Functional.java
@@ -0,0 +1,7 @@
+package Day18;	//Functional Interface
+
+@FunctionalInterface	//이걸쓰면 메소드가 하나있어야됨 
+public interface Functional {	//일반적으로 안씀,  void메소드가 여러개면 Functional에 빨간줄뜸
+	void method();	//Functional은 여러개의 메소드를 가질수있음	
+	
+}
