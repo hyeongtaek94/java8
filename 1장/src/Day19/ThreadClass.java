41022395b50b141cd821e5163446385fa0cf70d2
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-08 16:32:44 +0900

    19일차 : 쓰레드

---
+++ 1장/src/Day19/ThreadClass.java
@@ -0,0 +1,18 @@
+package Day19;
+
+public class ThreadClass extends Thread {
+	private String name;
+	
+	public ThreadClass(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				System.out.println(name + " ");
+			}
+		} catch(Exception e) {}
+	}
+}
---
+++ 1장/src/Day19/ThreadClassEx.java
@@ -0,0 +1,11 @@
+package Day19;
+
+public class ThreadClassEx {
+	public static void main(String[] args) {
+		Thread t1 = new ThreadClass("스레드1");
+		Thread t2 = new ThreadClass("스레드2");
+		
+		t1.start();
+		t2.start();
+	}
+}
