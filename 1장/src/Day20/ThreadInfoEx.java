081abf0870593793906e6737d98cdf5f4ddeea2a
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-09 13:08:05 +0900

    20일차 : 스레드 그룹 이름 얻기

---
+++ 1장/src/Day20/ThreadInfoEx.java
@@ -0,0 +1,21 @@
+package Day20;
+
+import java.util.Map;
+import java.util.Set;
+
+public class ThreadInfoEx {
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();	//데몬 스레드의 AutoSaveThread 클래스
+		autoSaveThread.setName("AutoSaveThread");
+		autoSaveThread.setDaemon(true);
+		autoSaveThread.start();
+		
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		Set<Thread> threads = map.keySet();
+		for(Thread thread : threads) {
+				System.out.println("Name: " + thread.getName() + ((thread.isDaemon())?"(데몬)": "(주)"));
+				System.out.println("\t" + "소속그룹: " + thread.getThreadGroup().getName());
+				System.out.println();
+		}
+	}
+}
