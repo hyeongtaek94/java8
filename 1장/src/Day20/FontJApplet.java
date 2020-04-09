c83984fb5f288edb26d02285524d8a22abe37605
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-09 15:11:26 +0900

    20일차 : 애플릿과 그래픽(폰트 설정)

---
+++ 1장/src/Day20/FontJApplet.java
@@ -0,0 +1,34 @@
+package Day20;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class FontJApplet extends JPanel{
+	Font sansPlain, serifBold, serifBoldI, monoItalic;
+	String param;
+	
+	public void init() {
+		serifBold = new Font("Serif", Font.BOLD, 10);
+		serifBoldI = new Font("Serif", Font.BOLD+Font.ITALIC, 13);
+		sansPlain = new Font("SanSerif", Font.PLAIN, 16);
+		monoItalic = new Font("Monospaced", Font.ITALIC, 20);
+	}
+	
+	public void paint(Graphics g) {
+		g.setFont(serifBold);
+		param = "Font name=Serif, style=BOLD, size=10";
+		g.drawString(param, 10, 10);
+		g.setFont(serifBoldI);
+		param = "Font name=Serif, style=BOLD+ITALIC, size=13";
+		g.drawString(param, 10, 50);
+		g.setFont(sansPlain);
+		param = "Font name=SansSerif, style=PLAIN, size=16";
+		g.drawString(param,  10,  90);
+		g.setColor(Color.blue);g.setFont(monoItalic);;
+		param = "Font name=Monospaced, style=ITALIC, size=20";
+		g.drawString(param, 10, 130);
+	}
+}
---
+++ 1장/src/Day20/FontJAppletEx.java
@@ -0,0 +1,16 @@
+package Day20;
+
+import javax.swing.JFrame;
+
+public class FontJAppletEx {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("폰트 설정 예제");
+		FontJApplet p = new FontJApplet();
+		p.init();
+		
+		f.setSize(600, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		f.add(p);
+		f.setVisible(true);
+	}
+}
