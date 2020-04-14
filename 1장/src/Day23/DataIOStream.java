0eb755d16322bab3ce4586f69d793bfc7efe2a32
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-14 15:20:28 +0900

    23일차 : 데이터 입출력 스트림

---
+++ 1장/src/Day23/DataIOStream.java
@@ -0,0 +1,31 @@
+package Day23;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataIOStream {
+	static String fname;
+	public static void main(String[] args) {
+		try {
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream("dsfile"));
+			dos.writeChar('c');
+			dos.writeInt(897);
+			dos.writeDouble(234.6);
+			dos.writeByte(34);	dos.writeBoolean(true);
+			dos.flush();	dos.close();
+			System.out.println("데이터 파일 입출력 : dsfile");
+			System.out.println("---------------------");
+			DataInputStream dis = new DataInputStream(new FileInputStream("dsfile"));
+			System.out.println(dis.readChar());
+			System.out.println(dis.readInt());
+			System.out.println(dis.readDouble());
+			System.out.println(dis.readByte());
+			System.out.println(dis.readBoolean());
+		}catch(IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
