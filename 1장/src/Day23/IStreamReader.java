e5c74cc1b6bff7b8e4b35bd91113c1a99d8c9258
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-14 14:29:10 +0900

    23일차 : 입출력 스트림 (문자 스트림)

---
+++ 1장/src/Day23/IStreamReader.java
@@ -0,0 +1,25 @@
+package Day23;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class IStreamReader {
+	static String fname;
+	public static void main(String[] args) {
+		int idata = -1;
+		Reader isr = new InputStreamReader(System.in);
+		System.out.println("키보드에서 문자입력[마지막에 엔터]");
+		System.out.println("-----------------------");
+		try {
+			while(true) {
+				idata = isr.read();
+				if(idata == -1)
+					break;
+				System.out.print((char)idata);
+			}
+		}catch(IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
