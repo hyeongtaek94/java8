757ab90a52f2bf54901556893fa4c5cf7e8f803a
hyeongtaek94 <hyeongtaek.choe@gmail.com>
2020-04-14 15:08:01 +0900

    23일차 : 파일입출력스트림

---
+++ 1장/src/Day23/FileIOStream.java
@@ -0,0 +1,28 @@
+package Day23;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileIOStream {
+	static String tfile;
+	public static void main(String[] args) {
+		int cnt;
+		char chr;
+		if(args.length !=1) {
+			System.out.println("매개변수로 텍스트파일 이름을 입력하세요");
+			System.exit(1);
+		}
+		tfile=args[0];
+		try {
+			FileReader fr = new FileReader(tfile);
+			System.out.println("파일 이름:" + tfile);
+			System.out.println("------------------");
+			while((cnt=fr.read()) != -1) {
+				chr = (char)cnt;
+				System.out.print(chr);
+			}
+		}catch(IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
